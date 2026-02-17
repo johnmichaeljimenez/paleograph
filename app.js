50018cb4c89178dfc04a608816c95c261be1e6dc
John <johnmichaeljimenez.thecmd@gmail.com>
2026-02-18 02:26:58 +0800

    Added file filtering and reading

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { question } from 'readline-sync'
-import { existsSync, readdirSync } from 'fs'
+import { existsSync, readdirSync, readFileSync } from 'fs'
 import path from 'path'
 
 const pathStr = question("Input path: ");
@@ -15,10 +15,39 @@ if (!existsSync(pathStr)) {
 
 console.log(pathStr);
 
+const validFiles = [
+	".txt",
+	".xml",
+	".html",
+	".css",
+	".js",
+	".json"
+];
+
+function isFileValid(file) {
+	const fileName = file.name.toLocaleLowerCase();
+	if (fileName === ".git" || fileName === "node_modules" || fileName === ".env" || fileName === "bin")
+		return false;
+
+	if (file.isDirectory())
+		return true;
+
+	let valid = false;
+
+	validFiles.forEach(element => {
+		if (path.extname(fileName) === element.toLocaleLowerCase())
+			valid = true;
+	});
+
+	return valid;
+}
+
 function getFilesRecursively(dir) {
 	let results = [];
 
-	const list = readdirSync(dir, { withFileTypes: true });
+	const list = readdirSync(dir, { withFileTypes: true }).filter(file => {
+		return isFileValid(file);
+	});
 
 	list.forEach((dirent) => {
 		const fullPath = path.join(dir, dirent.name);
@@ -35,3 +64,11 @@ function getFilesRecursively(dir) {
 
 const allFiles = getFilesRecursively(pathStr);
 console.log(allFiles);
+
+let mainText = "";
+allFiles.forEach(file => {
+	let txt = `===== ${file} =====`; //readFileSync(file);
+	mainText += `${txt}\n\n`;
+});
+
+console.log(mainText);
