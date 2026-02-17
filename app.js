72aa6eaad8e1fa9036cd8763c0cdae7b67081244
John <johnmichaeljimenez.thecmd@gmail.com>
2026-02-18 01:55:52 +0800

    Added directory scanning

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 import { question } from 'readline-sync'
-import { askLLM } from 'core-common'
+import { existsSync, readdirSync } from 'fs'
+import path from 'path'
 
-const a = question("Input: ");
-const b = await askLLM(a);
+const pathStr = question("Input path: ");
+if (!pathStr) {
+	console.error("empty");
+	process.exit(1);
+}
 
-console.log(b);
+if (!existsSync(pathStr)) {
+	console.error("invalid");
+	process.exit(1);
+}
+
+console.log(pathStr);
+
+function getFilesRecursively(dir) {
+	let results = [];
+
+	const list = readdirSync(dir, { withFileTypes: true });
+
+	list.forEach((dirent) => {
+		const fullPath = path.join(dir, dirent.name);
+
+		if (dirent.isDirectory()) {
+			results = results.concat(getFilesRecursively(fullPath));
+		} else if (dirent.isFile()) {
+			results.push(fullPath);
+		}
+	});
+
+	return results;
+}
+
+const allFiles = getFilesRecursively(pathStr);
+console.log(allFiles);
