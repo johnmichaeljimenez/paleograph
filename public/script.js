8cee649981fb29b793b0107c73b7246a628dc80c
John <johnmichaeljimenez.thecmd@gmail.com>
2026-02-20 23:30:31 +0800

    Request object integration (frontend)

--- public/script.js
+++ public/script.js
@@ -1,14 +1,27 @@
-import { newTemplate } from '/shared/request.js';
+import { newRequest, validateRequest } from '/shared/request.js';
 
 const form = document.getElementById('processForm');
+
+function loadData(req) {
+	for (const key in req) {
+		if (req.hasOwnProperty(key)) {
+			const field = form.querySelector(`[name="${key}"]`);
+			if (Array.isArray(req[key])) {
+				field.value = req[key].join("|");
+			} else {
+				field.value = req[key];
+			}
+		}
+	}
+}
+
+loadData(newRequest());
+
 form.addEventListener('submit', async (event) => {
 	event.preventDefault();
 
 	const formData = new FormData(form);
-	const data = { ...newTemplate(), ...Object.fromEntries(formData.entries()) };
-
-	data.whitelist = data.whitelist.split('|').map(item => item.trim());
-	data.blacklist = data.blacklist.split('|').map(item => item.trim());
+	const data = validateRequest({ ...Object.fromEntries(formData.entries()) });
 
 	try {
 		form.style.display = "none";
